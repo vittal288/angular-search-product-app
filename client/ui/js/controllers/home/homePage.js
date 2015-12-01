327482b017c0511226bfc26824b1ad85a01a4668
unknown <vittalkam@IN-L-MTR4189178.raet.local>
2015-12-01 16:28:16 +0530

    updated code and csv file in refData folder

--- client/ui/js/controllers/home/homePage.js
+++ client/ui/js/controllers/home/homePage.js
@@ -1,10 +1,9 @@
 aroCityApp.controller('homePage',function($scope,$rootScope,$http,restAPI){	
     
     //call rest api 		        	                          
-    var restApi = restAPI.get({method:'fetchHomeBrandTilesInfo',cityName:'Bengaluru'},function(){      
+    var restApi = restAPI.get({method:'fetchHomeBrandTilesInfo',cityName:'Bangalore'},function(){      
 	     $rootScope.homeTiles = restApi.json;       
-    });
-     
+    });     
 });
 
 
